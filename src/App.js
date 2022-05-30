3455d36b8a9faa9fa368f680ff004a9f96545ecf
MiaKim0804 <kimmichino0804@gmail.com>
2022-05-30 13:55:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import config from './config/index';
+import { Form } from './components/Form';
 
 function App() {
   return (
-    
+    <>
       <header className="App-header">
         <div>BaseURL : {config.baseURL}</div>
       </header>
-  
+      <Form />
+    </>
+    
+      
   );
 }
 
